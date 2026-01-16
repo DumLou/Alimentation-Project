a4494d05c3cb3044ad8caa17635019564091ba6a
Dumont Louison <louison.dumont@psl.eu>
2026-01-16 01:21:04 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 from recommender import recommend_products
+import os
 
 app = Flask(__name__)
 
 # Home route 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     results = []
@@ -19,7 +19,7 @@ def index():
                 results=[],
                 error="Veuillez entrer un nom de produit."
             )
-
+        # Optional parameters
         brand = request.form.get("brand") or None
         nutriscore = request.form.get("nutriscore") or None
         label = request.form.get("label") or None
@@ -27,7 +27,7 @@ def index():
 
         # Numerical parameters
         try:
-            similarity_level = int(request.form.get("similarity_level", 5))
+            similarity_level = int(request.form.get("similarity_level", 5)) # 1 to 5
             label_weight = float(request.form.get("label_weight", 1.0))
             nutri_weight = float(request.form.get("nutri_weight", 1.0))
             env_weight = float(request.form.get("env_weight", 1.0))
@@ -53,13 +53,14 @@ def index():
             env_weight=env_weight,
             top_n=10
         )
-
+        # Process results
         if df_results is not None and not df_results.empty:
             results = df_results.to_dict(orient="records")
 
     return render_template("index.html", results=results)
 
 
-# Run the app
+# Run the app (Render compatible)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render port
+    app.run(host="0.0.0.0", port=port)
